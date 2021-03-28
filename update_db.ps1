4996d86c56e59a1825a765717ce82e7267f9b929
Wes Phooprasert <wes.dev.acc@gmail.com>
2021-03-28 10:55:18 -0400

    Updated database migration script

--- update_db.ps1
+++ update_db.ps1
@@ -1,8 +1,15 @@
 if (Test-Path migrations){
-    echo "removing"
+    echo "Removing Migrations Folder"
     rm -Recurse -Confirm:$false -Force migrations
 }
 
+python API/drop_tables.py -t alembic_version
+
+echo "Initializing Migrations"
 python API/migrate.py db init
+
+echo "Migrating Tables"
 python API/migrate.py db migrate
+
+echo "Upgrading DB"
 python API/migrate.py db upgrade
